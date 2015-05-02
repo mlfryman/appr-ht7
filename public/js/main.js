828d48ba97702f41e2cc991996d8652df65421d4
Conlan Craft <conlanc@patientfocus.com>
2015-05-02 13:54:25 -0500

    starfield, thing

--- public/js/main.js
+++ public/js/main.js
@@ -7,10 +7,14 @@ require.config({
 
 require(
     [
-        'game'
+        'game',
+        'states/menu',
+        'states/sprint'
     ],
-    function(game) {
+    function(game, Menu, Sprint) {
         'use strict';
-        game.run();
+        game.state.add('menu', new Menu(game));
+        game.state.add('sprint', new Sprint(game));
+        game.state.start('menu');
     }
 );
